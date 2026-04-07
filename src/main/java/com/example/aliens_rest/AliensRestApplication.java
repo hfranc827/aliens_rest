98abf4c2ec7f2eae3ab078cf672fb1f598760822
htr mont <sunefranco3@gmail.com>
2026-04-07 13:23:49 -0500

    Initialize Spring Boot project with Maven wrapper, configuration files, and basic application structure

---
+++ src/main/java/com/example/aliens_rest/AliensRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.aliens_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AliensRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AliensRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aliens_rest/AliensRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aliens_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliensRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
